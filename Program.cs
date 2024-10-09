10f02addff02b4874b2de6bc4aa4c23bb4fc5c9e
Lorilei-Forbear-Student <207830@student.oaisd.org>
2024-10-09 14:12:44 +0000

    added some questions and some variables.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, User!")
+       Console.WriteLine("What is your name?");
+       string name = Console.ReadLine();
+       Console.WriteLine("How old are you?");
+       string age = Console.ReadLine();
+       Console.WriteLine("What color are your eyes?");
+       string eyeColor = Console.ReadLine();
+       Console.WriteLine("What color is your hair?");
+       string hairColor = ();
+       
+    }
+}
